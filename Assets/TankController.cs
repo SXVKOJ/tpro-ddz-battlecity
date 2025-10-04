ab26999f98b6a69c8ab4b146673c211db2ca9737
243113 <243113@k733.edu>
2025-10-04 19:38:34 +0300

    fix

--- Assets/TankController.cs
+++ Assets/TankController.cs
@@ -1,21 +1,28 @@
 using UnityEngine;
+using UnityEngine.UIElements;
 
-public interface
 public class Tank : MonoBehaviour
 {
     public float speed = 5.0f;
-    private Rigidbody rb;
+    private Rigidbody2D rb;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        rb = GetComponent<RigidBody>();
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            float mv = Input.GetAxis("Vertical");
+            Vector2 mov = new Vector2(0.0f, mv);
+            rb.AddForce(mov * speed);
 
+            Debug.Log("Mov W");
+        }
     }
 
     void OnCollisionEnter(Collision collision)
